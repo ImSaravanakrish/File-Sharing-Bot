8f2f52324d41600f8a7ae1c4cc2ca774493fa5b7
Saravanakrish <68471536+ImSaravanakrish@users.noreply.github.com>
2021-12-07 22:47:29 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -105,8 +105,7 @@ async def start_command(client: Client, message: Message):
                 id = message.from_user.id
             ),
             reply_markup = reply_markup,
-            disable_web_page_preview = True,
-            quote = True
+            disable_web_page_preview = True        
         )
         return
 
