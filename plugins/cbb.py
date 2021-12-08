33ae50d1073a2072d4795901ba1b76e61f40f09c
Saravanakrish <68471536+ImSaravanakrish@users.noreply.github.com>
2021-12-08 13:29:19 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -1,4 +1,4 @@
-#(©)Codexbotz ✯
+#(©)Codexbotz
 
 from pyrogram import __version__
 from bot import Bot
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : @CodeXBotz\n○ Support Group : @CodeXBotzSupport</b>",
+            text = f"<b>✯ ᴄʀᴇᴀᴛᴏʀ : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n✯ ʟᴀɴɢᴜᴀɢᴇ : <code>Python3</code>\n✯ ʟɪʙʀᴀʀʏ : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n✯ ꜱᴏᴜʀᴄᴇ ᴄᴏᴅᴇ : <a href='https://github.com/imsaravanakrish'>ᴄʟɪᴄᴋ ʜᴇʀᴇ</a>\n✯ ᴜᴘᴅᴀᴛᴇꜱ ᴄʜᴀɴɴᴇʟ : @TamilBots\n✯ ꜱᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ : @TamilSupport</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
