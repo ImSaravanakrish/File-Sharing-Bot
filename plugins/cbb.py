e9f8c33e0f9530b61b08f560681405e09a8e6e88
Saravanakrish <68471536+ImSaravanakrish@users.noreply.github.com>
2021-12-08 13:56:26 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -2,8 +2,10 @@
 
 from pyrogram import __version__
 from bot import Bot
-from config import OWNER_ID
+from config import OWNER_ID,START_MSG
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+from database.sql import add_user, query_msg, full_userbase
+
 
 @Bot.on_callback_query()
 async def cb_handler(client: Bot, query: CallbackQuery):
@@ -16,9 +18,33 @@ async def cb_handler(client: Bot, query: CallbackQuery):
                 [
                     [
                         InlineKeyboardButton("🔒 Close", callback_data = "close")
+                        InlineKeyboardButton("Back", callback_data = "Back")
                     ]
                 ]
             )
+        )
+       
+    elif data == "Back":
+        buttons = [[
+            InlineKeyboardButton('ᴊᴏɪɴ ᴍʏ ᴍᴏᴠɪᴇ ɢʀᴏᴜᴘ', url=f'http://t.me/TamilMoviesChat')
+            ],[
+            InlineKeyboardButton('ꜱᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ', url='https://t.me/TamilSupport''),
+            InlineKeyboardButton('🤖 Updates', url='https://t.me/TamilBots')
+            ],[
+            InlineKeyboardButton('😊 About Me", callback_data = "about'),
+            InlineKeyboardButton('🔒 Close", callback_data = "close')
+        ]]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        await query.message.edit_text(
+            caption=START_MSG.format(
+                first = message.from_user.first_name,
+                last = message.from_user.last_name,
+                username = None if not message.from_user.username else '@' + message.from_user.username,
+                mention = message.from_user.mention,
+                id = message.from_user.id
+            ),            
+            reply_markup=reply_markup,
+            parse_mode='html'
         )
     elif data == "close":
         await query.message.delete()
