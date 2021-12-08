7df8a1e710d4def4f73d3aff9a5291d898689681
Saravanakrish <68471536+ImSaravanakrish@users.noreply.github.com>
2021-12-08 14:44:31 +0530

    Update and rename script to script.py

---
+++ script.py
@@ -0,0 +1,14 @@
+class script(object):
+    START_MESSAGE = """ʜᴇʟʟᴏ {first}
+ɪ ᴄᴀɴ ꜱᴛᴏʀᴇ ᴘʀɪᴠᴀᴛᴇ ꜰɪʟᴇꜱ ɪɴ ꜱᴘᴇᴄɪꜰɪᴇᴅ ᴄʜᴀɴɴᴇʟ ᴀɴᴅ ᴏᴛʜᴇʀ ᴜꜱᴇʀꜱ ᴄᴀɴ ᴀᴄᴄᴇꜱꜱ ɪᴛ ꜰʀᴏᴍ ꜱᴘᴇᴄɪᴀʟ ʟɪɴᴋ
+    
+ᴍᴀɪɴ ᴄʜᴀɴɴᴇʟ » @TamilMovies4K
+ᴍᴏᴠɪᴇ ʀᴇQᴜᴇꜱᴛ ɢʀᴏᴜᴘ » @TamilMoviesChat"""
+
+    ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
+┏━━━━━━━━━❥
+┣ Cʀᴇᴀᴛᴇʀ -> <a href=https://t.me/SaravanaKrish>✭ Iϻsαi🎭Arⱥ𝖘aภ ✭</a>
+┣ Uᴘᴅᴀᴛᴇꜱ -> @TamilBots
+┣ 𝖲ᴜᴘᴘᴏʀᴛ -> @TamilSupport
+┗━━━━━━━━━❥
+"""
