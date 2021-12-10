c65cc32b5fd69d8c33f6599d154e8a01697e7dd9
Saravanakrish <68471536+ImSaravanakrish@users.noreply.github.com>
2021-12-10 14:31:48 +0530

    Create script.py

---
+++ script.py
@@ -0,0 +1,25 @@
+class Scripted(object):    
+  
+  
+ ABOUT_TEXT = """
+╭────[🔅Rᴇɴᴀᴍᴇʀ Bᴏᴛ🔅]───⍟
+│
+├<b>🤖 Bot Name : <a href='https://t.me/Renamer_teleroid_bot'>Movie Bot</a></b>
+│
+├<b>📢 Channel : <a href='https://t.me/TamilBots'>TamilBots</a></b>
+│
+├<b>👥 Support Chat : <a href='https://t.me/TamilSupport'>TamilSupport</a></b>
+│
+├<b>💢 Source : <a href='https://github.com/imsaravanakrish'>Click Here</a></b>
+│
+├<b>🌐 Server : <a href='https://heroku.com'>Heroku</a></b>
+│
+├<b>📕 Library : <a href='https://github.com/pyrogram'>Pyrogram 1.2.8</a></b>
+│
+├<b>㊙ Language: <a href='https://www.python.org'>Python 3.9.4</a></b>
+│
+├<b>👨‍💻 Developer : <a href='https://t.me/SaravanaKrish'>✭ Iϻsαi🎭Arⱥ𝖘aภ ✭</a></b>
+│
+├<b>🚸 Powered By : <a href='https://t.me/TamilBots'>TamilBotZ</a></b>
+│
+╰──────[Thanks 😊]───⍟"""
